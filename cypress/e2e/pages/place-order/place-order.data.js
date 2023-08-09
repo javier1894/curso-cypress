eed31b719861e91b889037530f306d2464214826
Javier GM <javiergranadosmartin@gmail.com>
2023-08-09 17:37:04 +0200

    creacion de pagina place order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData {
+  static get testData() {
+    return {
+      name: "Test name",
+      country: "España",
+      city: "Madrid",
+      creditCard: 4111111111111111,
+      month: 12,
+      year: 2026,
+    };
+  }
+}
